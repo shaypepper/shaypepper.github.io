d6b03c0a3da15d42932edeb3408fce1bc4a31338
Shay Culpepper <shaypepper@gmail.com>
2017-05-19 12:16:12 -0400

    Added four-color styling

--- calcudoku/index.js
+++ calcudoku/index.js
@@ -4,6 +4,7 @@ var activeSquare = {
   i: 0,
   j: 0
 };
+var group = {};
 var candidateMode = false;
 var square = () => $("#s"+activeSquare.i+activeSquare.j);
 var answer = () => $("#a"+activeSquare.i+activeSquare.j);
@@ -16,7 +17,7 @@ var ops = [
 
 function catchKeyStroke(x){
   let X = x.keyCode;
-  console.log(X);
+  console.log(X, N+48);
   if (X >= 37 && X <= 40) {
     if (X == 38 && activeSquare.i){
       activeSquare.i--;
@@ -43,7 +44,7 @@ function catchKeyStroke(x){
       }
     }
   } else if (X == 32) {
-    candidateMode = !candidateMode;
+    switchMode(true);
   } else if (X == 8) {
     if (answer().text()) {
       answer().text("");
@@ -51,6 +52,18 @@ function catchKeyStroke(x){
       $(".active .candidates").removeClass("selected-candidate");
     }
   }
+}
+
+function switchMode(spacebar){
+  if (spacebar) {
+    candidateMode = !candidateMode;
+  } else if ($(this).attr("data-mode") == "candidate") {
+    candidateMode = true;
+  } else {
+    candidateMode = false;
+  }
+  $(".mode-btn").removeClass("btn-selected");
+  $("[data-mode="+(candidateMode?"candidate]":"guess]")).addClass("btn-selected").blur();
 }
 
 function endOfTurnCheck(){
@@ -85,6 +98,7 @@ function print(puzzle){
 }
 
 function generatePuzzle(){
+  group = {};
   var puzzle = [];
   // Generate randum numbers
   for (r=0; r<N; r++){
@@ -123,6 +137,12 @@ function generatePuzzle(){
         continue;
       }
       g++;
+      group[g] = {
+        connections: [],
+        color: 0,
+        colorOptions: [],
+        members: []
+      };
       var I = i;
       var J = j;
       var size = Math.random();
@@ -135,7 +155,9 @@ function generatePuzzle(){
       }
 
       var digits = [];
+      var bg = [true, true, true, true];
       while (size) {
+        group[g].members.push([I,J])
         digits.push(puzzle[I][J].digit);
         puzzle[I][J].group = g;
         size--;
@@ -145,6 +167,23 @@ function generatePuzzle(){
         if (I < N-1 && !puzzle[I+1][J].group) options.push([I+1, J]);
         if (J > 0 && !puzzle[I][J-1].group)   options.push([I, J-1]);
         if (J < N-1 && !puzzle[I][J+1].group) options.push([I, J+1]);
+
+        if (I > 0 && puzzle[I-1][J].group && puzzle[I-1][J].group != g && (group[g].connections.indexOf(puzzle[I-1][J].group) == -1))   {
+          group[g].connections.push(puzzle[I-1][J].group);
+          group[puzzle[I-1][J].group].connections.push(g);
+        }
+        if (I < N-1 && puzzle[I+1][J].group && puzzle[I+1][J].group != g && (group[g].connections.indexOf(puzzle[I+1][J].group) == -1)) {
+          group[g].connections.push(puzzle[I+1][J].group);
+          group[puzzle[I+1][J].group].connections.push(g);
+        }
+        if (J > 0 && puzzle[I][J-1].group && puzzle[I][J-1].group != g && (group[g].connections.indexOf(puzzle[I][J-1].group) == -1))   {
+          group[g].connections.push(puzzle[I][J-1].group);
+          group[puzzle[I][J-1].group].connections.push(g);
+        }
+        if (J < N-1 && puzzle[I][J+1].group && puzzle[I][J+1].group != g && (group[g].connections.indexOf(puzzle[I][J+1].group) == -1)) {
+          group[g].connections.push(puzzle[I][J+1].group);
+          group[puzzle[I][J+1].group].connections.push(g);
+        }
 
         if (options.length) {
           [I, J] = options[Math.floor(Math.random()*options.length)];
@@ -164,6 +203,31 @@ function generatePuzzle(){
       puzzle[i][j].hint = ops[Math.floor(Math.random()*nOps)](digits);
     }
   }
+  console.log(group);
+  // figure out colors
+  var g = puzzle[Math.floor(N/2)][Math.floor(N/2)].group;
+  group[g].color = 1;
+  var stack = [].concat(group[g].connections);
+  while (stack.length) {
+    console.log(g, stack);
+    let color = group[g].color;
+    var g = stack.shift();
+    if (!group[g].color) {
+      let colors = [false, true, true, true, true];
+      group[g].connections.forEach(function(x, i){
+        if (stack.indexOf(x) == -1 && !group[x].color) {
+          stack.push(x);
+        } else if (group[x].color) {
+          colors[group[x].color] = false;
+        }
+      });
+      colors.forEach((x,i) => { if (x) group[g].colorOptions.push(i); });
+      console.log(g, colors, group[g].colorOptions);
+      group[g].color = group[g].colorOptions[Math.floor(Math.random()*group[g].colorOptions.length)]  || ((Number(color)+2) % 4) + 1;
+      console.log(g,'BECOMES',group[g].color)
+    }
+  }
+
   return puzzle;
 }
 
@@ -176,19 +240,28 @@ function drawPuzzle(n){
   document.body.style.setProperty('--N', N);
   $("#puzzle").empty();
 
-  // setup
+  // setup 
   for (r=0; r<N; r++){
-    $("#puzzle").append('<tr id="r'+r+'" class="row"></tr>');
-    var rowId = '#r' + r;
+    let row = $('<tr>', { id: 'r'+r, class:"square-row" });
+    $("#puzzle").append(row);
     for (s=0; s<N; s++){
-      var border = (r<N-1 && puzzle[r][s].group != puzzle[r+1][s].group) ? "bottom-border" : "";
+      let border = (r<N-1 && puzzle[r][s].group != puzzle[r+1][s].group) ? " bottom-border" : "";
       border += (s<N-1 && puzzle[r][s].group != puzzle[r][s+1].group) ? " right-border" : "";
-      $(rowId).append('<td id="s'+r+s+'" class="square '+  border +'"><p id="a'+r+s+'" class="answer"></p><div><table><tr><td class="hint" colspan="3">'+puzzle[r][s].hint+'</td></tr></table></div></td>');
+      let square = $('<td>', { id:'s'+r+s, class: 'square'+border+' bg'+ group[puzzle[r][s].group].color });
+      let answer = $('<p>', { id:'a'+r+s, class: 'answer' });
+      let hint = $('<div><table><tr><td class="hint" colspan="3">'+
+        puzzle[r][s].hint+'</td></tr></table></div>');
+      row.append(square);
+      square.append(answer);
+      square.append(hint);
+      let candidateRow = $("<tr>", { class: "candidate-row" });
       for (n=1; n<=N; n++){
         if (!((n-1) % 3)) {
-          $('#s' + r + s +' table ').append('<tr class="candidate-row"></tr>');
+          candidateRow = $("<tr>", { class: "candidate-row" });
+          square.find("table").append(candidateRow);
         }
-        $('#s' + r + s +' .candidate-row:last-child').append('<td class="candidates c'+n+'">'+n+'</td>');  
+        let candidate = $("<td>", { class: "candidates c"+n }).text(n);
+        candidateRow.append(candidate);  
       }
     }
   }
@@ -203,12 +276,17 @@ function drawPuzzle(n){
   $("#s00").addClass("active");
 };
 
-$(document).ready(function(){
+$(function(){
   drawPuzzle();
 
   $(".size-btn").click(function(){
-    drawPuzzle($(this).attr("data-n"));
+    drawPuzzle(Number($(this).attr("data-n")));
+    $(this).blur();
+    $(".size-btn").removeClass("btn-selected");
+    $(this).addClass("btn-selected");
   });
+
+  $(".mode-btn").click(switchMode);
 
   $(document).keyup(catchKeyStroke);
 });
